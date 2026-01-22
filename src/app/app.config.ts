a22286e1f5e74eeb3cb8af95965570889c999503
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-01-22 12:18:45 -0500

    Iconos globales añadidos a la app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { NZ_I18N, es_ES } from 'ng-zorro-antd/i18n';
+import { provideNzIcons } from 'ng-zorro-antd/icon'; // Solo necesitas el provider aquí
+
+import {
+  UserOutline,
+  LockOutline,
+  IdcardOutline,
+  KeyOutline,
+  EyeOutline,
+  EyeInvisibleOutline,
+} from '@ant-design/icons-angular/icons';
 
 import { routes } from './app.routes';
 
+// Lista de iconos disponibles en toda la app
+const icons = [
+  UserOutline,
+  LockOutline,
+  IdcardOutline,
+  KeyOutline,
+  EyeOutline,
+  EyeInvisibleOutline,
+];
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    { provide: NZ_I18N, useValue: es_ES },
+    provideNzIcons(icons), // Registra los iconos globalmente
+  ],
 };
--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -4,7 +4,9 @@ import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angula
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-login',
@@ -14,14 +16,15 @@ import { NzInputModule } from 'ng-zorro-antd/input';
     NzFormModule,
     NzInputModule,
     NzButtonModule,
-    NzCheckboxModule
+    NzCheckboxModule,
+    NzModalModule,
+    NzIconModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
- private fb = inject(NonNullableFormBuilder);
+  private fb = inject(NonNullableFormBuilder);
 
   validateForm = this.fb.group({
     username: this.fb.control('', [Validators.required]),
@@ -32,12 +35,29 @@ export class Login {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
         }
       });
     }
+  }
+
+  // Modal olvido de contraseña
+  isVisible = false;
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
   }
 }
