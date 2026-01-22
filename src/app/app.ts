d60abf7be44600f94de7d9399bc5c319b06a6c32
Christian Gallegos <christian@dev.com>
2026-01-21 23:57:55 -0500

    Login componente terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzFlexModule } from 'ng-zorro-antd/flex';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, NzInputModule, NzIconModule, NzFlexModule, NzButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  readonly passwordVisible = signal(false);
-  readonly password = signal('');
+ 
 }
